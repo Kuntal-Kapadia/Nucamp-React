94f46bda350225c890c3ff1a042afb9af9ebd464
Kuntal <kuntalkapadia1@gmail.com>
2020-06-16 09:44:09 -0700

    React Animation Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -98,12 +99,18 @@ render(){
 function RenderCampsite({campsite}){
     return (
         <div className="col-md-5 m-1">
+             <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
             <Card>
             <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
                 <CardBody>
                     <CardText>{campsite.description}</CardText>
                 </CardBody>
             </Card>
+            </FadeTransform>
         </div>
     );
 }
@@ -112,14 +119,18 @@ function RenderComments({comments, addComment, campsiteId}) {
     return(
         <div className="col-md-5 m-1">
             <h4>Comments</h4>
+            <Stagger in>
             {comments.map(comment=>
-                    <div key={comment.id}>
+                    <Fade in key={comment.id}>
+                        <div>
                         <p>{comment.text}<br></br>
                         --{comment.author},{" "}
                         {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                    </div>     
+                    </div>   
+                    </Fade>  
                 )
             }
+            </Stagger>
             <CommentForm campsiteId={campsiteId} addComment={addComment} />
         </div>
     );   
