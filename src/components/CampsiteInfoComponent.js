baebc01b0fd56bab9336579613f7b9f921ddcb1c
Kuntal <kuntalkapadia1@gmail.com>
2020-06-20 20:35:26 -0700

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -30,7 +30,7 @@ class CommentForm extends Component{
         // alert("Current state is: " + JSON.stringify(values));
         alert(this.props.campsiteId + JSON.stringify(values));
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
     
 render(){
@@ -119,7 +119,7 @@ function RenderCampsite({campsite}){
     );
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     return(
         <div className="col-md-5 m-1">
             <h4>Comments</h4>
@@ -135,7 +135,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                 )
             }
             </Stagger>
-            <CommentForm campsiteId={campsiteId} addComment={addComment} />
+            <CommentForm campsiteId={campsiteId} postComment={postComment} />
         </div>
     );   
 }
@@ -178,7 +178,7 @@ function CampsiteInfo(props){
                         <RenderCampsite campsite={props.campsite} />
                         <RenderComments 
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                     />
                     </div>
